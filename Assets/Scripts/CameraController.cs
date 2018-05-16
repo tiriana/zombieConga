d0aa3628d65aeb504c1da04ade63606afc0c40dd
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-05-17 00:52:45 +0200

    LAst part started

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float speed = 1f;
+    private Vector3 newPosition;
+
+    // Use this for initialization
+    void Start () {
+        newPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        newPosition.x += Time.deltaTime * speed;
+        transform.position = newPosition;
+    }
+}
