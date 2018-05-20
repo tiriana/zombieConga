43d176c845152269380f560b64e49d4b3a567186
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-05-20 23:40:54 +0200

    spawning cats

---
+++ Assets/Scripts/KittyCreator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittyCreator : MonoBehaviour
+{
+    //1
+    public float minSpawnTime = 0.75f;
+    public float maxSpawnTime = 2f;
+
+    //2    
+    void Start()
+    {
+        Invoke("SpawnCat", minSpawnTime);
+    }
+
+    //3
+    void SpawnCat()
+    {
+        Debug.Log("TODO: Birth a cat at " + Time.timeSinceLevelLoad);
+        Invoke("SpawnCat", Random.Range(minSpawnTime, maxSpawnTime));
+    }
+}
